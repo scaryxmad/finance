1fa64d408d780da368932326559af3eb04ab2871
ScArY <tselmennm@gmail.com>
2021-02-16 15:00:14 +0800

    shibaal

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var uiController = (function () {
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
@@ -21,19 +21,23 @@ var uiController = (function () {
   };
 })();
 //sanhuutei ajillah controller //2dahi controller
+//private data
 var financeController = (function () {
+  //private data//
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-  var Exprense = function (id, description, value) {
+  //private data
+  var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+  //private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: [],
     },
@@ -42,20 +46,39 @@ var financeController = (function () {
       exp: 0,
     },
   };
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 //programmiin holbogch controller //3dahi controller
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     //1. Oruulah ogodloliig delgetsees olj avna.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
     //2. olj avsan ogodluudee sanhuugiin controllert damjuulj tend hadgalna
+    financeController.addItem(input.type, input.description, input.value);
     //3. Olj avsan ogodluudee web deeree tohiroh hesegt ni gargana
     //4. tosviig tootsoolno.
     //5.Etsesiin uldegdel tootsoog delgetsend gargana
   };
   var setupEventListeners = function () {
     var DOM = uiController.getDOMstrings();
-    document.querySelector(".add__btn").addEventListener("click", function () {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
     });
     document.addEventListener("keypress", function (event) {
